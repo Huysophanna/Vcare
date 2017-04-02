dab6554680eb265b2d29932bf719d8cd706ddde4
Rana <rana@Ranas-MacBook-Pro.local>
2017-04-02 14:44:18 +0700

    New codes calling API

--- Assets/Scripts/UserDataScript.cs
+++ Assets/Scripts/UserDataScript.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.UI;
+using UnityEngine.Networking;
+using LitJson;
+
 
 public class UserDataScript : MonoBehaviour {
 
@@ -13,11 +16,14 @@ public class UserDataScript : MonoBehaviour {
 	private string userBirthMonth = "";
 	private int userBirthMonthIndex;
 	private int daysInMonth;
-
+	private JsonData Data;
+	private JsonData Json;
+	private JsonData List;
 	List<string> birthYear = new List<string>();
 	List<string> birthDay = new List<string>();
 
 	void Start() {
+		
 		InitializeBirthSelection ();
 		//make sure that the input field is not empty, drag and drop in the editor
 		Assert.IsNotNull (userHeight);
@@ -32,11 +38,53 @@ public class UserDataScript : MonoBehaviour {
 
 	public void SaveData() {
 		// TODO: Save data into storage
-		Debug.Log (userHeight.text);
-		Debug.Log (userWeight.text);
+//		Debug.Log (userHeight.text);
+//		Debug.Log (userWeight.text);
+		StartCoroutine(APIcall());
+
 	}
 
+	public IEnumerator APIcall()
+	{
+		string URL = "https://api.nutritionix.com/v1_1/search";
 
+		string jsonData = "";
+		jsonData = "{\"appId\":\"56f421e1\",\"appKey\":\"24b1553f1e6a835b8e66c3cbc62a7820\",\"queries\":{\"item_name\":\"Kids Fries\",\"brand_name\":\"MacDonalds\"}}";
+
+		var request = new UnityWebRequest(URL, "POST");
+		byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonData);
+		request.uploadHandler = (UploadHandler) new UploadHandlerRaw(bodyRaw);
+		request.downloadHandler = (DownloadHandler) new DownloadHandlerBuffer();
+		request.SetRequestHeader("Content-Type", "application/json");
+
+		yield return request.Send();
+
+		if (request.isError)
+		{
+			Debug.Log(request.error);
+		}
+		else
+		{
+			// Show results as text
+//			Debug.Log("Returning:" + request.downloadHandler.text);
+//			Data = JsonMapper.ToObject(request.downloadHandler.);
+//			Debug.Log(Data);
+//			Json =  JsonUtility.ToJson(request.downloadHandler.text);
+			Data = JsonMapper.ToObject(request.downloadHandler.text);
+		    
+			foreach (JsonData record in Data["hits"]) {
+				Debug.Log(record.ToString());
+			}
+
+//			for (int i = 0; i < Data["hits"].Count; i++) {
+//				Debug.Log (Data["hits"][i]["fields"]["brand_name"]);
+//			}
+
+//			Debug.Log (List);
+//						JSONObject j = new JSONObject(request.downloadHandler.text);
+
+		}
+	}
 
 
 
