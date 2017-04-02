3bff5d3379f7eee1f5aa33e766011ebc62da105c
kenphanith <phanithken@gmail.com>
2017-04-02 11:05:20 +0700

    Auto Typing Text

---
+++ Assets/Scripts/PercentageCalculation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PercentageCalculation : MonoBehaviour {
+
+	public Image healthBar;
+
+	// Use this for initialization
+	void Start () {
+		//healthBar.fillAmount = 0.5f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
